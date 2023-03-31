1e1d9142ad81285119c97cf53aaaa9149a2f272f
muthu ram <mr032495@gmail.com>
2023-03-31 22:45:49 +0530

    map

--- pages/map.dart
+++ pages/map.dart
@@ -1,11 +1,10 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
-import 'keys.dart';
+import '../keys.dart';
 
 class MapSample extends StatefulWidget {
   const MapSample({Key? key}) : super(key: key);
@@ -19,8 +18,9 @@ class MapSampleState extends State<MapSample> {
 
   static const LatLng sourceLocation = LatLng( 37.33500926,-122.03272188);
   static const LatLng destination = LatLng( 37.33429383,-122.06600055);
-List<LatLng> polylineCoordinates =[];
-LocationData? currentLocation;
+  static const LatLng destination2 = LatLng( 37.33609383,-122.07600055);
+List<LatLng> polylineCoordinates =[LatLng( 37.33500926,-122.03272188),LatLng( 37.33429383,-122.06600055),LatLng( 37.33609383,-122.07600055)];
+var  currentLocation;
 
 void getCurrentLocation(){
   Location location = Location();
@@ -28,14 +28,17 @@ void getCurrentLocation(){
   location.getLocation().then((location) {
     currentLocation = location;
   });
+  print(location.getLocation());
+  print("currentLocation");
   print(currentLocation);
 }
+
   void getPolyPoints() async{
   print("object");
     PolylinePoints polylinePoints = PolylinePoints();
     
     PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
-        "AIzaSyCGShAceyIm1LHL2mLja0eKCKDjoZV2RzY",
+        google_api_key,
       PointLatLng(sourceLocation.latitude, sourceLocation.longitude),
       PointLatLng(destination.latitude, destination.longitude),
     );
@@ -54,9 +57,10 @@ void getCurrentLocation(){
   @override
   void initState(){
   getCurrentLocation();
-    getPolyPoints();
+    // getPolyPoints();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -66,9 +70,7 @@ void getCurrentLocation(){
           style: TextStyle(color: Colors.black,fontSize: 16),
         ),
       ),
-      body:currentLocation == null
-          ? Center(child: Text("Loading"))
-          : GoogleMap(
+      body:GoogleMap(
         initialCameraPosition: CameraPosition(
             target: sourceLocation,
             zoom: 13.5
@@ -78,19 +80,24 @@ void getCurrentLocation(){
               polylineId : PolylineId("route"),
             points: polylineCoordinates,
             color: Colors.black,
-            width: 6
+            width: 3
           )
-
         },
         markers: {
-
           Marker(
             markerId: MarkerId("source"),
             position: sourceLocation,
           ),
           Marker(
+            markerId: MarkerId("source"),
+            position: destination2,
+          ),
+          Marker(
             markerId: MarkerId("destination"),
             position: destination,
+            infoWindow: InfoWindow(
+              title: "Destination"
+            )
           ),
         },
 
